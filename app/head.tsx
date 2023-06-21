3ed7e941c913d584e9dd4462ff58ecbc9c7a6d57
Abduqodirov Muhammadxon <110591779+MrTitanHearted@users.noreply.github.com>
2023-06-21 19:06:57 +0500

    starter template

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+import icon from "next/image"
+
+export default function Head() {
+    return <>
+        <title>Hooray MTD</title>
+        <meta name='viewport' content='width=device-width, inital-scale=1' />
+        <meta name='description' content='Hooray MTD app' />
+        <link rel="icon" href="/favicon.ico" />
+    </>
+}
